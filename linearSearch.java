2de468521b515c03e5ad60fe33c2fed714abd844
Abhishek Behera <96872894+AbhishekBehera20@users.noreply.github.com>
2023-03-04 20:27:24 +0530

    Update linearSearch.java

--- linearSearch.java
+++ linearSearch.java
@@ -13,7 +13,7 @@ public class linearSearch {
    }
 
    public static void main(String args[]) {
-    int numbers[] = {2,4,6,10,12,14,16};
+    int numbers[] = {2,4,6,8,10,12,14,16};
     int key = 10;
 
     int index = linearsearch(numbers , key);
