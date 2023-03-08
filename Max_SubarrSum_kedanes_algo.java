6c2a6bc1b1dff6e074f206b5f28a6a6714a40194
Abhishek Behera <96872894+AbhishekBehera20@users.noreply.github.com>
2023-03-08 18:17:05 +0530

    Add files via upload

---
+++ Max_SubarrSum_kedanes_algo.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Max_SubarrSum_kedanes_algo {
+    public static void kadanes(int numbers[]) {
+          int ms = Integer.MIN_VALUE;
+          int cs = 0;
+
+          for(int i = 1;i<numbers.length;i++) {
+                cs = cs + numbers[i];
+                if(cs < 0) {
+                    cs = 0;
+                }
+                ms = Math.max(cs , ms);
+            }
+
+            System.out.println("our max subarray sum is : " + ms);
+
+    }
+
+    public static void main(String args[]) {
+        int numbers[] = {-2,-3,4,-1,-2,1,5,-3};
+        kadanes(numbers);
+    }
+}
