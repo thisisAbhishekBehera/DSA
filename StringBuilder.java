10f3d6225935e4bebd20e8a32c01c77511159445
Abhishek Behera <96872894+AbhishekBehera20@users.noreply.github.com>
2023-03-15 11:04:52 +0530

    Create StringBuilder.java

---
+++ StringBuilder.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Abhi {
+    public static void main(String args[]) {
+        StringBuilder str = new StringBuilder();
+        for(char ch = 'a'; ch<='z'; ch++) {
+            str.append(ch);
+        }
+
+        System.out.println(str);
+    }
+}
