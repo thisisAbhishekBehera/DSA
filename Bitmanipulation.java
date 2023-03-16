6d2904864021f3e958ac4e1a4811d03740ceede5
Abhishek Behera <96872894+AbhishekBehera20@users.noreply.github.com>
2023-03-16 23:19:27 +0530

    Add files via upload

---
+++ Bitmanipulation.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Bitmanipulation {
+     public static void main(String args[]) {
+        System.out.println((5 & 6));//AND
+        System.out.println((5 | 6));//OR
+        System.out.println((5 ^ 6));//xor
+        System.out.println((~5));//1s compliment 
+     }
+}
