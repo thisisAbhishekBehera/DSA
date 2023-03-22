eeee18c4ba9d98f6f1266703ce86ec5e0227b315
Abhishek Behera <lutun2025@gmail.com>
2023-03-22 17:30:41 +0530

    Update Basic_Sorting.java

--- Basic_Sorting.java
+++ Basic_Sorting.java
@@ -17,6 +17,25 @@ public class Basic_Sorting {
         }
         System.out.println("No of Swaps : " + swaps);
     }
+    
+    //Bubble sort optimised 
+    public static void modifiedBubbleSort(int arr[]) {
+        for(int turn = 0;turn<arr.length-1; turn ++) {
+            boolean swapped = false;
+            for(int j=0;j<arr.length-1-turn; j++) {
+                if(arr[j] > arr[j+1]) {
+                    //swap
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    swapped = true;
+                }
+            }
+            if(swapped == false) {
+                break;
+            }
+        }
+    }
 
     public static void printArr(int arr[]) {
         for(int i = 0; i<arr.length;i++) {
